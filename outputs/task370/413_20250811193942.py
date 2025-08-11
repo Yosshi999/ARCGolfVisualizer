b298d513a15f81b1cf1a71d777075f5071817013
satos <satos@fos.kuis.kyoto-u.ac.jp>
2025-08-11 20:20:02 +0900

    nyan

---
+++ outputs/task370/413_20250811193942.py
@@ -0,0 +1,3 @@
+i=lambda v,k:v[k]if 0<=k<len(v)else[-1]
+e=enumerate
+p=lambda g:(b:=g[0][0],c:=sum(set(sum(g,[])))-b,h:=len(g),y:=[c in v for v in g].index(1),x:=g[y].index(c),d:=max(filter(lambda p: 0<=p[1]+y<h and i(i(g,p[1]+y),p[2]+x)==0,[*sum((((x,x,-x),(x,-x,x),(x,x,x),(x,-x,-x))for x in range(9)),())])),p:=0,q:=0,*((p:=p+d[1],q:=q+d[2],g:=[[a if i(i(g,y+p),x+q)else c for x,a in e(v)]for y,v in e(g)])for _ in'.'*9),g)[-1]
