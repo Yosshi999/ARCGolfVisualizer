aff1131d178493bff0fa4fde040a390eed4ec84d
satos <satos@fos.kuis.kyoto-u.ac.jp>
2025-08-12 15:50:36 +0900

    hoo

---
+++ outputs/task349/369_20250812154853.py
@@ -0,0 +1,5 @@
+t=lambda g:[*zip(*g)]
+e=enumerate
+r=range
+h=lambda g:(s:={0},[[(w:=0,[w:=d//2 for d in r(50)if d*(9,)in[v[x:x+d],v[x-d+1:x+1]]],[s:=s|{(q,p)}for p in r(x-w,x+w+1) for q in r(y-w,y+w+1)if w])for x,a in e(v)]for y,v in e(g)],[[3 if(y,x)in s and a<9else a for x,a in e(v)]for y,v in e(g)])[2]
+p=lambda g:h(t([[a|1if 9in v[:x]else a for x,a in enumerate(v)]for v in t(g)]))
