d34cd8a07aad7c136bcd0febd191cea802d8473b
satos <satos@fos.kuis.kyoto-u.ac.jp>
2025-09-01 00:35:19 +0900

    nyu

---
+++ outputs/task378/236_20250901003503.py
@@ -0,0 +1,8 @@
+e=enumerate
+def f(g):
+ for y,v in e(g):
+  for x,_ in e(v):
+   d=sum([w[x:x+3:2]for w in g[y:y+3:2]],[]);Y=y
+   while x>0<Y>0<all(d)<len(d)>2>d.count(d[3]):x-=1;Y-=1;g[Y][x]=d[3]
+ return[*map(list,zip(*g))][::-1]
+p=lambda g:f(f(f(f(g))))
