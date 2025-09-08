7b893b2f779c8bd3d00e2ee037ceaddb248a7db5
sz-223 <vakejakublog@protonmail.com>
2025-09-08 19:46:49 +0900

    wei

---
+++ outputs/task325/249_20250908194506.py
@@ -0,0 +1,5 @@
+R=range
+def p(g):
+ W=len(g[0]);A=len(G:=sum(g,[]));L=[-~i*G[i] for i in R(A)]
+ for x in R(A):L=[{L[x+(-~x%W!=0<G[x+1]==G[x])]:L[x],L[x+W*(A>x+W>0<G[x+W]==G[x])]:L[x]}.get(_,_)for _ in L]
+ n=len(set(L))-1;return [[8*(y==x)for x in R(n)]for y in R(n)]
