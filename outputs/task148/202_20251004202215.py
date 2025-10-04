6ece346f44f06f096b859d7ab6313cfe88f7e6a3
Yosshi999 <Yosshi999@users.noreply.github.com>
2025-10-04 20:22:32 +0900

    hoge

---
+++ outputs/task148/202_20251004202215.py
@@ -0,0 +1,9 @@
+def f(g):
+ Y=[]
+ for v in g:
+  if v[0]:
+   if sum(v)==10:c=v.index(8);v[1:c+1]=[8]*~-c+[4]
+   Y+=8in v,
+  if v[-1]&2and Y and Y.pop(0):v[:-1]=[8]*(len(v)-1)
+ return[v[::-1]for v in g]
+p=lambda g:f(f(g))
