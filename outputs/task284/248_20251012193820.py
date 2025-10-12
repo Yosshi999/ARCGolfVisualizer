f52321f48b80b4cd927e295177b61c86ca4e3f0b
satos <satos@fos.kuis.kyoto-u.ac.jp>
2025-10-13 01:47:55 +0900

    nyan

---
+++ outputs/task284/248_20251012193820.py
@@ -0,0 +1,6 @@
+e=enumerate
+def f(g):
+ i=g.index(V:=max(g))
+ if len({*V})>2:m,M=[j for j,w in e(V)if w];C=m+M>>1;g=[[V[(m,M)[x>C]]*(((y==i)*(m<=x<=M)|(-3<y-i<3)*(-2<x-C<3))^(-2<y-i<2)*(-1<x-C<2))for x,c in e(v)]for y,v in e(g)]
+ return[*zip(*g)]
+p=lambda g:f(f(g))
