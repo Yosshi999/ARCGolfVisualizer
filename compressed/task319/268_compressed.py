40a2a296700ed599b02ea3565d1933e983affdae
sh-mug <52068539+sh-mug@users.noreply.github.com>
2025-10-22 04:16:14 +0900

    uo-

---
+++ compressed/task319/268_compressed.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes("•OEZ1ÞÏ)²L†wèIúá¤Ï›÷UpîÎ´ƒ.YEó6Þ„‰ ÙÛå}êîÅÃ…N;è•³M³Ü£s˜YÈk„Ìf$”•Ä›,8®…ãßµ§™‡h­^l8½¨/Zü/äÙ.%PÃ¶/œê^Ù;½dëVíµNmÔŸÖÕ, ê©}æä¤GÄlV´µµË(FkCÃ{ù§Ûw¢óíó¥|íõÎN£äCîÌÎ'rCœt1šnø—Ù{áµ¥(-è(	_Kñ","L1"),-8))
