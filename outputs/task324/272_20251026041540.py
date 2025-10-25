a2a17447d9b2710fa7d2563bf1e30cb07ed8ef39
cuminum <88176718+cuminum@users.noreply.github.com>
2025-10-26 04:16:25 +0900

    wai

---
+++ outputs/task324/272_20251026041540.py
@@ -0,0 +1,9 @@
+e=enumerate
+def p(g):
+ C=sorted(G:=sum(g,m:=[]),key=G.count)[:2]
+ for y,v in e(g+[*zip(*g)]):
+  for x,c in e(v):
+   if c in C:
+    if len({*v})==2:u=v;t=sum(C)-c,c
+    if y<len(g):m+=y+x,y-x-99
+ return[[any({y+x,y-x-99}&{*m})*t[a in u]or a for x,a in e(v)]for y,v in e(g)]
