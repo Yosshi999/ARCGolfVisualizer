4b06b882b4cd47c8e425d0cd05eb512cfef1ca4e
Yosshi999 <Yosshi999@users.noreply.github.com>
2025-10-29 10:29:00 +0000

    gcp

---
+++ compressed/task089/265_compressed.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes('UAk!…ïýg6ca»·ûG†9¬]ºE](„ü÷V¹½Ï÷TÞó¦¬98˜|ÛœW?ð3fdÑïE³òªK{—Ñ«ÄAØŠ1›tc†œ«Ùâwƒz$à3@ZaµªvH]½#ºž.4x=ã4¥SÃûŸpÐŽ1«ÌV:^¨ýãuÊœä¦¯P©àMFÒ½$UR1ó-FlxIgyi\0Ö˜¹|-8õÛB,ˆçâÚQ2s}®Áeñù€<‡‘?','L1'),-8))
